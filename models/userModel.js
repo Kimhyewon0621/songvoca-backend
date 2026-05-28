76c5b59740e415bc5418ef226b59b7596c556e94
Minhee Jeong <jungminhee0730@naver.com>
2026-05-29 00:06:54 +0900

    added userModel.js file that inserts a new user into users table

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const pool = require('./db');
+
+class EmailAlreadyExistError extends Error {
+  constructor(email) {
+    super(`E-mail adress "${email}" is already in use.`);
+    this.name = "EmailAlreadyExistError";
+  }
+}
+
+async function create({email, password_hash, name}){ // return the new id
+    try{
+        const result = await pool.query(
+            'INSERT INTO users (email, password_hash, name) VALUES ($1, $2, $3) RETURNING id',
+            [email, password_hash, name]
+        );
+    }catch(err){
+        if(err.code === 23505){
+            throw new EmailAlreadyExistError(email);
+        }
+        throw err ;
+    }
+}
