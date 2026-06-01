1d6d9328dd8a1c846f994904cf4e687b8a69a7fc
Minhee Jeong <jungminhee0730@naver.com>
2026-06-01 20:43:50 +0900

    Trying remove pool.connect to resolve error

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const {pool, initDb} = require('./db');
 const PORT = process.env.PORT || 8080;
 
 async function start(){
+    try{
     await initDb();
 
     // 데이터베이스 연결 테스트 함수
-    pool.connect((err, client, release) => {
-        if (err) {
-            return console.error('PostgreSQL 연결 실패:', err.stack);
-        }
-        console.log('PostgreSQL 연결 성공! (songvoca_db)');
-        release(); // 연결 해제(반환)
-    });
+    await pool.query('SELECT 1');
+    console.log('PostgreSQL 연결 성공! (songvoca_db)');
 
     app.listen(PORT, () =>{
         console.log(`our project if listening on port ${PORT}`);
     });
+    }catch(err){
+        console.error('서버 시작 중 DB 연결 실패:', err);
+    }
+
 }
 
 start();
