d94328736dd324b39906c34d7a851f4b8910c236
Hyewon <mykhw123@naver.com>
2026-06-08 00:13:51 +0900

    fix: remove study_status and add DISTINCT to public songs

--- db.js
+++ db.js
@@ -1,19 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-/*const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-});*/
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
 });
 
 pool.on('error', (err) => console.error('Unexpected error on idle client', err));
